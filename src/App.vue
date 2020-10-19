fc9195b01f0424ca2d0a68d949ed35cbd0e96db0
shane carvalho <shanec_@hotmail.com>
2020-10-20 01:28:01 +1100

    feat(iniital commit): initial Commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <StartPage />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import StartPage from "./components/StartPage.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    StartPage
+  }
+});
+</script>
+
+<style lang="scss"></style>
