a053c0decb9ad1ffdf7120f78311daf559344d01
shane carvalho <shanec_@hotmail.com>
2020-10-20 23:37:57 +1100

    feat(startpage.vue): initial Design Consideration

--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -1,4 +1,18 @@
-<template><div>placeholder</div></template>
+<template>
+  <div class="parent flex">
+    <div class="child">
+      test1
+    </div>
+    <div class="child">
+      <ul>
+        <li>tst</li>
+      </ul>
+    </div>
+    <div class="child">
+      test3
+    </div>
+  </div>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
@@ -10,3 +24,22 @@ export default defineComponent({
   }
 });
 </script>
+
+<style lang="scss">
+.parent.flex {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #282828;
+  height: 100vh;
+}
+
+.child {
+  min-width: 200px;
+  min-height: 200px;
+  background-color: #928374;
+  //color:#928374;
+  margin: 5px;
+  padding: 5px;
+}
+</style>
